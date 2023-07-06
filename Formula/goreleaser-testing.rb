e5132f705c3a7b28e05881387100317dc49a877f
goreleaserbot <goreleaserbot@theleo.zone>
2023-07-06 06:03:21 -0700

    Brew formula update for goreleaser-testing version v0.13.0

--- Formula/goreleaser-testing.rb
+++ Formula/goreleaser-testing.rb
@@ -5,11 +5,11 @@
 class GoreleaserTesting < Formula
   desc "Goreleaser testing description"
   homepage "https://github.com/robinovitch61/goreleaser-testing"
-  version "0.12.0"
+  version "0.13.0"
 
   on_macos do
-    url "https://github.com/robinovitch61/goreleaser-testing/releases/download/v0.12.0/goreleaser-testing_0.12.0_Darwin_all.tar.gz"
-    sha256 "2662201662ddecbad466034ecf70ca191311e744fce46d2ce7caea56a91a226c"
+    url "https://github.com/robinovitch61/goreleaser-testing/releases/download/v0.13.0/goreleaser-testing_ 0.13.0_Darwin_all.tar.gz"
+    sha256 "695f48eca6dd09333388b2b0d58eedc64466fb0579522c8ecd7cc44551ad5fa8"
 
     def install
       bin.install "goreleaser-testing"
@@ -18,16 +18,16 @@ class GoreleaserTesting < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/robinovitch61/goreleaser-testing/releases/download/v0.12.0/goreleaser-testing_0.12.0_Linux_x86_64.tar.gz"
-      sha256 "279b7138c359085bbbe1e276d00dd6864862f71cf422e2bfe646637f57177816"
+      url "https://github.com/robinovitch61/goreleaser-testing/releases/download/v0.13.0/goreleaser-testing_ 0.13.0_Linux_x86_64.tar.gz"
+      sha256 "e6fcf957851ed878d7fcd3b29c767aba7b6f3c28f2b9ac32dc960470a76c7179"
 
       def install
         bin.install "goreleaser-testing"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/robinovitch61/goreleaser-testing/releases/download/v0.12.0/goreleaser-testing_0.12.0_Linux_arm64.tar.gz"
-      sha256 "d526895f0df289e91454ac3f11c084af3eb151b1dfbd4cf9fb2bc1083f50b386"
+      url "https://github.com/robinovitch61/goreleaser-testing/releases/download/v0.13.0/goreleaser-testing_ 0.13.0_Linux_arm64.tar.gz"
+      sha256 "7789697220d46b46b0ac4e651fd62c20e8a8f9860b2af1db8d61e24916d2eaef"
 
       def install
         bin.install "goreleaser-testing"
