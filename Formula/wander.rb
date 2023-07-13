f7e72172cc8a8855582eaa3178ba06caaaa901f4
goreleaserbot <goreleaserbot@theleo.zone>
2023-07-13 09:56:37 +0100

    Brew formula update for wander version v0.11.1

--- Formula/wander.rb
+++ Formula/wander.rb
@@ -5,11 +5,11 @@
 class Wander < Formula
   desc "A terminal app/TUI for HashiCorp Nomad"
   homepage "https://github.com/robinovitch61/wander"
-  version "0.11.0"
+  version "0.11.1"
 
   on_macos do
-    url "https://github.com/robinovitch61/wander/releases/download/v0.11.0/wander_0.11.0_Darwin_all.tar.gz"
-    sha256 "c9fe1a586cb81e161e9909ea7edab91894d0b38c2e77c049719507c9164e4086"
+    url "https://github.com/robinovitch61/wander/releases/download/v0.11.1/wander_0.11.1_Darwin_all.tar.gz"
+    sha256 "8d6aa800d4e307cfb85dce0c2fbc30026fcd7a120f2184ecdf878c284a1ea12e"
 
     def install
       bin.install "wander"
@@ -17,17 +17,17 @@ class Wander < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/robinovitch61/wander/releases/download/v0.11.0/wander_0.11.0_Linux_x86_64.tar.gz"
-      sha256 "3aacbbb1f8804ab226b9edd61b679b1bf362fa8865efc20366e44a288622f206"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/robinovitch61/wander/releases/download/v0.11.1/wander_0.11.1_Linux_arm64.tar.gz"
+      sha256 "d1656f5439fd4777e4e1daccdd26402f857793d37c2f9b541b9c2e735f2e46ee"
 
       def install
         bin.install "wander"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/robinovitch61/wander/releases/download/v0.11.0/wander_0.11.0_Linux_arm64.tar.gz"
-      sha256 "0c44f97e696fd4cc7407ab9fe89177b5273f1785a085881ca000c86b9a635ef6"
+    if Hardware::CPU.intel?
+      url "https://github.com/robinovitch61/wander/releases/download/v0.11.1/wander_0.11.1_Linux_x86_64.tar.gz"
+      sha256 "b05e0243f054aacc9addd2c83af380a5269f74134c490bfaf950e4bed8021ef3"
 
       def install
         bin.install "wander"
