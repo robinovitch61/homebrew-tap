ecb92866e3cc826f1e28d0addb3459f46af7e495
goreleaserbot <goreleaserbot@theleo.zone>
2023-12-30 17:05:50 -0800

    Brew formula update for goreleaser-testing version v0.33.0

--- Formula/goreleaser-testing.rb
+++ Formula/goreleaser-testing.rb
@@ -5,11 +5,11 @@
 class GoreleaserTesting < Formula
   desc "Goreleaser testing description"
   homepage "https://github.com/robinovitch61/goreleaser-testing"
-  version "0.32.0"
+  version "0.33.0"
 
   on_macos do
-    url "https://github.com/robinovitch61/goreleaser-testing/releases/download/v0.32.0/goreleaser-testing_0.32.0_Darwin_all.tar.gz"
-    sha256 "1ca08026485073715242a0d0827cfc7d3dafdf8f62ab6d50de63320050e02c24"
+    url "https://github.com/robinovitch61/goreleaser-testing/releases/download/v0.33.0/goreleaser-testing_0.33.0_Darwin_all.tar.gz"
+    sha256 "24267abea4e0b773a0c7b4a538ca586c33a3b1be22a40066ac5af861e84a9422"
 
     def install
       bin.install "goreleaser-testing"
@@ -17,17 +17,17 @@ class GoreleaserTesting < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/robinovitch61/goreleaser-testing/releases/download/v0.32.0/goreleaser-testing_0.32.0_Linux_x86_64.tar.gz"
-      sha256 "a5952d1c457a83d8f5a8d65e9c56c07545b114f147c318b62d20a3384115cfb0"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/robinovitch61/goreleaser-testing/releases/download/v0.33.0/goreleaser-testing_0.33.0_Linux_arm64.tar.gz"
+      sha256 "38eee4bc8dc8bf33bab3343858835cf0b89a5927aa28a8389ff1b54dc97fa6e2"
 
       def install
         bin.install "goreleaser-testing"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/robinovitch61/goreleaser-testing/releases/download/v0.32.0/goreleaser-testing_0.32.0_Linux_arm64.tar.gz"
-      sha256 "13c129350419054c2ab096b7e87872287a72170212dc490ab8cd3ced42f1d988"
+    if Hardware::CPU.intel?
+      url "https://github.com/robinovitch61/goreleaser-testing/releases/download/v0.33.0/goreleaser-testing_0.33.0_Linux_x86_64.tar.gz"
+      sha256 "135492641604d3d4d566054511d352683807d8f9da2127002e2dbbb58483737b"
 
       def install
         bin.install "goreleaser-testing"
