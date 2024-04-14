72627930e785d1c30d5b0c9f4e8f1771672c441d
Leo Robinovitch <leorobinovitch@gmail.com>
2024-04-13 22:48:42 -0700

    Brew formula update for webtoon-dl version v0.0.9

--- Formula/webtoon-dl.rb
+++ Formula/webtoon-dl.rb
@@ -5,11 +5,11 @@
 class WebtoonDl < Formula
   desc "A cli for downloading content from webtoon.com"
   homepage "https://github.com/robinovitch61/webtoon-dl"
-  version "0.0.8"
+  version "0.0.9"
 
   on_macos do
-    url "https://github.com/robinovitch61/webtoon-dl/releases/download/v0.0.8/webtoon-dl_0.0.8_Darwin_all.tar.gz"
-    sha256 "4de083dec4d6c840fd7ed1bbec5f3f76134654bde267c22289e9aa5dfaa052f6"
+    url "https://github.com/robinovitch61/webtoon-dl/releases/download/v0.0.9/webtoon-dl_0.0.9_Darwin_all.tar.gz"
+    sha256 "6dd419a2278cb1a737506f15c90256558bc1db481d0defbdce0da472c5761384"
 
     def install
       bin.install "webtoon-dl"
@@ -18,16 +18,16 @@ class WebtoonDl < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/robinovitch61/webtoon-dl/releases/download/v0.0.8/webtoon-dl_0.0.8_Linux_x86_64.tar.gz"
-      sha256 "d8d2b6367ba420cd07693a52d69beeb093bdcc4598a0ed5550de6ac5c386b2dc"
+      url "https://github.com/robinovitch61/webtoon-dl/releases/download/v0.0.9/webtoon-dl_0.0.9_Linux_x86_64.tar.gz"
+      sha256 "e53ad5c0dfaf0b998daa20b6b9a1febe72af989ed46d186c9236c1951ba83409"
 
       def install
         bin.install "webtoon-dl"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/robinovitch61/webtoon-dl/releases/download/v0.0.8/webtoon-dl_0.0.8_Linux_arm64.tar.gz"
-      sha256 "7a16a1c20301cf0f424fbc997e439353208c693e107e1cbd751d00b22136f9c0"
+      url "https://github.com/robinovitch61/webtoon-dl/releases/download/v0.0.9/webtoon-dl_0.0.9_Linux_arm64.tar.gz"
+      sha256 "048a2d2d9748bf72383f3caceba50063e809347b631239d03ec86d8468fe3915"
 
       def install
         bin.install "webtoon-dl"
